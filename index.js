6e66dab58dc4d6f86fb9d6e44821d281bc5b68b6
tratnayake <t.ratnayake1@gmail.com>
2015-03-12 17:09:55 -0700

    Cleaned up and ready to go

--- index.js
+++ index.js
@@ -1,42 +1,81 @@
 var net = require('net');
+var fs = require('fs');
+var lineReader = require('line-reader');
 
-//Get comand line arguments
-//To run use node index.js ssh 192.168.0.3 22 9022
-var arguments = process.argv.slice(2);
-var ip = arguments[1];
-var service = arguments[0];
-var listenPort = arguments[2];
-var remotePort = arguments[3];
+var services = new Array();
 
-// To send data out
-  var server = net.createServer(function(conn){
+//MAIN//
+//Read from config file
+lineReader.eachLine(__dirname+'/configfile.txt', function(line, last) {
 
-    var client = net.createConnection(remotePort,ip,
-      function(){
-        console.log(service + " client connected to the server!");
-        }
-    );
+  if(line[0] !="/"){
+    arguments = line.split(",");
+    console.log("The arguments are "+arguments);
+    var ip = arguments[1];
+    var service = arguments[0];
+    var listenPort = arguments[2];
+    var remotePort = arguments[3];
 
-    //To get Data In
-    client.on('data',function(data){
-      //console.log(server);
-      conn.write(data);
-    });
+    var serviceElement = {ip: ip, service: service, listenPort: listenPort, remotePort: remotePort};
+    services.push(serviceElement);
 
-  console.log(service + " client connected");
 
-  conn.on('end',function(){
-    console.log(service + " client disconnected");
-  })
+  }
 
-  conn.on('data',function(data){
-    client.write(data);
+  if(last){
+    console.log("Start the listening servers and begin forwarding...");
 
-  })
+    // Start listening & then start the server
+    for (var i = 0; i < services.length; i++) {
+      var serviceElement = services[i];
+      listenPackets(serviceElement.remotePort,serviceElement.ip,serviceElement.service,serviceElement.listenPort,startPortServer);
+    }
+
+
+
+  }
 });
+
+
+function listenPackets(remotePort,ip,service,listenPort,startPortServer){
+
+  // To send data out
+    var server = net.createServer(function(conn){
+
+      var client = net.createConnection(remotePort,ip,
+        function(){
+          console.log(service + " client connected to the server!");
+          }
+      );
+
+      //To get Data In
+      client.on('data',function(data){
+        //console.log(server);
+        conn.write(data);
+      });
+
+    console.log(service + " client connected");
+
+    conn.on('end',function(){
+      console.log(service + " client disconnected");
+    })
+
+    conn.on('data',function(data){
+      client.write(data);
+
+    })
+  });
+
+startPortServer(server,service,listenPort);
+
+}
 
 
 //Start the server
-server.listen(listenPort,function(){
-  console.log(service + " server listening on port 8080..");
-});
+function startPortServer(server,service,listenPort){
+  server.listen(listenPort,function(){
+    console.log(service + " server listening on port " + listenPort + " ..");
+  });
+
+
+}
